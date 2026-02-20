f3c3e60dec686a4e16c6c72c1f76318ba64c84c7
Ronny Endara <rojaence@gmail.com>
2026-02-19 23:17:18 -0500

    add: Layout and children routes in country

--- src/app/country/country.route.ts
+++ src/app/country/country.route.ts
@@ -1,10 +1,21 @@
 import { Route } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayoutComponent } from './layouts/country-layout/country-layout.component';
 
 const countryRoutes: Route[] = [
   {
     path: '',
-    component: ByCapitalPageComponent
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital'
+      }
+    ]
   }
 ];
 
---
+++ src/app/country/layouts/country-layout/country-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-country-layout',
+  imports: [RouterModule],
+  templateUrl: './country-layout.component.html',
+  styles: ``
+})
+export class CountryLayoutComponent {
+
+}
