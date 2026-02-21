4c3244b11030d2e7813af3eeb809111055274471
Ronny Endara <rojaence@gmail.com>
2026-02-21 12:26:49 -0500

    update: Join capital names in country mapper

--- src/app/country/mappers/country.mapper.ts
+++ src/app/country/mappers/country.mapper.ts
@@ -9,7 +9,7 @@ export class CountryMapper {
       flag: restCountry.flag,
       flagSvg: restCountry.flags.svg,
       name: restCountry.name.official,
-      capital: restCountry.capital[0],
+      capital: restCountry.capital.join(', '),
       population: restCountry.population
     }
   }
